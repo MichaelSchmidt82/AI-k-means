1d0787077a3e981ce394ff37a2df4d6e944ee764
Michael Schmidt <Michael.Schmidt82@gmail.com>
2018-05-20 16:33:26 -0500

    cancer script seems to correctly pass parameters

--- cancer.sh
+++ cancer.sh
@@ -5,11 +5,9 @@ cluster_max=95
 data_file="datasets/cancer-data.txt"
 out="cancer-results.txt"
 
-echo $@
-
 echo "Clustering has started."
 for ((cluster=1; cluster<=$cluster_max; cluster++)); do
-	./scripts/cancer-pass.sh "$cluster"
+	./scripts/cancer-pass.sh "$cluster" $@
 	echo "finished clustering ($cluster)"
 done
 
